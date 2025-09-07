98be80c14fc3dc3ad55658a11f90355dda4d60ed
99Kalana <wksp990518@gmail.com>
2025-09-07 13:34:50 +0530

    add a notification handler that listens for when a user taps a notification,

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React ,{useEffect} from 'react';
 import { Slot, Stack, useRouter } from 'expo-router';
 import { AuthProvider } from '@/context/AuthContext';
 import { LoaderProvider } from '@/context/LoaderContext';
@@ -9,7 +9,21 @@ const RootLayout = () => {
 
     const router = useRouter();
 
-    
+    useEffect(() => {
+        // Set up the listener for notification responses
+        const responseListener = Notifications.addNotificationResponseReceivedListener(response => {
+            const { plantId } = response.notification.request.content.data;
+            if (plantId) {
+                // Navigate to the correct plant details screen
+                router.push(`/(dashboard)/plants/${plantId}`);
+            }
+        });
+
+        // Clean up the listener when the component unmounts
+        return () => {
+            Notifications.removeNotificationSubscription(responseListener);
+        };
+    }, []);
 
     return (
         <LoaderProvider>
