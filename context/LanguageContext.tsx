273a260fa6846ab9d3d217b3808811ed76dae63a
99Kalana <wksp990518@gmail.com>
2025-09-11 14:02:37 +0530

    Language cintext updated for several languages incuding English, Spanish, French, German

--- context/LanguageContext.tsx
+++ context/LanguageContext.tsx
@@ -4,6 +4,7 @@ import React, { createContext, useState, useContext } from 'react';
 const LanguageContext = createContext({
     language: 'English',
     changeLanguage: (newLanguage: string) => {},
+    availableLanguages: ['English', 'Spanish', 'French', 'German']
 });
 
 // Create a custom hook to easily access the context
@@ -12,12 +13,13 @@ export const useLanguage = () => useContext(LanguageContext);
 // Provider component that manages the state and provides it to children
 export const LanguageProvider = ({ children }: { children: React.ReactNode }) => {
     const [language, setLanguage] = useState('English');
+    const availableLanguages = ['English', 'Spanish', 'French', 'German'];
 
     const changeLanguage = (newLanguage: string) => {
         setLanguage(newLanguage);
     };
 
-    const value = { language, changeLanguage };
+    const value = { language, changeLanguage, availableLanguages };
 
     return (
         <LanguageContext.Provider value={value}>
