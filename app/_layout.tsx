90671ab3c53405c2243cab801418c1f341a4f004
99Kalana <wksp990518@gmail.com>
2025-09-16 08:36:27 +0530

    code organized in main layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -12,16 +12,16 @@ const RootLayout = () => {
     const router = useRouter();
 
     useEffect(() => {
-        // Set up the listener for notification responses
+        
         const responseListener = Notifications.addNotificationResponseReceivedListener(response => {
             const { plantId } = response.notification.request.content.data;
             if (plantId) {
-                // Navigate to the correct plant details screen
+                
                 router.push(`/(dashboard)/plants/${plantId}`);
             }
         });
 
-        // Clean up the listener when the component unmounts
+        
         return () => {
             Notifications.removeNotificationSubscription(responseListener);
         };
